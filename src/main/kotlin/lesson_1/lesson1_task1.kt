6781c725f011144d5ff4b1097462864bed64a84c
Kolya <kolia047@mail.ru>
2024-03-14 23:02:18 +0500

    KS-1-1 First task

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,7 +2,7 @@ package lesson_1
 
 fun main() {
     val theNumberOfOrders: Int = 1;
-    val textOfGratitude: String = "Thank you for your purchase"
+    val textOfGratitude: String = "Thank you for your purchase."
 
 //Забыл создать ветку
 }
