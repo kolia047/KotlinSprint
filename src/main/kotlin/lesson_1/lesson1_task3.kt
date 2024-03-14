6ec00fb2c0e8e853b3679610daf7ae0bdc323321
Kolya <kolia047@mail.ru>
2024-03-14 23:38:35 +0500

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val yearOfSpaceFlight: Int = 1961
+    var hour = 9
+    var minute = 7
+
+    println(yearOfSpaceFlight)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print("$hour:$minute")
+}
