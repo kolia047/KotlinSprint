6e8eb9f5eb41bf3d4dbba730c40d0caee2c2c6be
Kolya <kolia047@mail.ru>
2024-03-15 20:44:04 +0500

    KS-1-3 corrected

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,16 +1,16 @@
 package lesson_1
 
 fun main() {
-    val yearOfSpaceFlight: Int = 1961
-    var hour = 9
-    var minute = 7
+    val yearOfSpaceFlight: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(yearOfSpaceFlight)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print("$hour:$minute")
 }
