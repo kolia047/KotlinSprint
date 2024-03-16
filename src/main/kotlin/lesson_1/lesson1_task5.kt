393bd23341cfc0f7c7825b1d09294e627d996422
Kolya <kolia047@mail.ru>
2024-03-16 20:52:57 +0500

    KS-1-5 added constants

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,14 @@
 package lesson_1
 
+const val NUMBER_OF_SECONDS_IN_A_HOUR = 3600
+const val NUMBER_OF_SECONDS_IN_A_MINUTE = 60
+
 fun main() {
+
     val numberOfSeconds = 6480
-    val seconds: Int = numberOfSeconds % 60
-    val minute: Int = (numberOfSeconds / 60) % 60
-    val hour: Int = numberOfSeconds / 3600
+    val seconds: Int = numberOfSeconds % NUMBER_OF_SECONDS_IN_A_MINUTE
+    val minute: Int = (numberOfSeconds / NUMBER_OF_SECONDS_IN_A_MINUTE) % NUMBER_OF_SECONDS_IN_A_MINUTE
+    val hour: Int = numberOfSeconds / NUMBER_OF_SECONDS_IN_A_HOUR
     val result = String.format("%02d:%02d:%02d", hour, minute, seconds)
 
     println(result)
