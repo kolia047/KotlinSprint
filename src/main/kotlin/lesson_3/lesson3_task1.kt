09f688645bf70c108d3d3f22c8c9965f2a5ffeb3
Kolya <kolia047@mail.ru>
2024-03-17 01:10:26 +0500

    KS-3-1

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val name = "Piter"
+    var welcome = "Welcome. Good morning"
+
+    println("$welcome $name")
+    welcome = "Welcome. Good evening"
+    println("$welcome $name")
+
+}
